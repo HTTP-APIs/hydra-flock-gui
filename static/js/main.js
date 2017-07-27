f435df77446223cf86f823268c692c20d8bbcb9e
Akshay Dahiya <xadahiya@gmail.com>
2017-07-27 23:41:00 +0530

    Refactor, modified central server location algo

--- static/js/main.js
+++ static/js/main.js
@@ -1,103 +1,173 @@
-function get_area_of_interest() {
+
+//var earth_radius = 3960.0
+var earth_radius = 6371.0;
+var degrees_to_radians = Math.PI/180.0;
+var radians_to_degrees = 180.0/Math.PI;
+
+function change_in_latitude(distance){
+  // Given a distance north, return the change in latitude.
+  return (distance/earth_radius)*radians_to_degrees
+
+}
+
+function change_in_longitude(latitude, distance){
+  // Given a latitude and a distance west, return the change in longitude.
+  // Find the radius of a circle around the earth at given latitude.
+  r = earth_radius*Math.cos(latitude*degrees_to_radians)
+  return (distance/r)*radians_to_degrees
+
+}
+
+function get_central_controller_location(){
+  // Get coordinates of the central controller.
+  return [-10.040397656836609, -55.03373871559225]
+}
+
+function update_central_controller_location(location){
+  // Upate the coordinates of central controller using AJAX post
+}
+
+
+function convert_direction_to_north_or_west(distance_moved, direction){
+  // Convert East and South direction to North and East.
+  if( direction == "S"){
+    distance_moved = distance_moved * -1;
+    direction = "N";
+  }
+  else if (direction == "E"){
+    distance_moved = distance_moved* -1;
+    direction = "W";
+  }
   return {
-    "lower_left": [-30.040397656836609, -30.03373871559225],
-    "upper_right": [-10.040248585302038, -60.03993927003302]
+    "distance_moved": distance_moved,
+    "direction": direction
   }
 }
 
-function gen_square(area_of_interest) {
-  var lower_left, upper_right;
 
-  lower_left = area_of_interest["lower_left"];
-  upper_right = area_of_interest["upper_right"]
-  var path = [];
-  // console.log(lower_left, upper_right);
-  path.push(lower_left)
-  path.push([lower_left[0], upper_right[1]])
-  path.push(upper_right)
-  path.push([upper_right[0], lower_left[1]])
+function gen_new_coordinates_from_change_in_coordinates(old_coordinates, change_in_coordinates){
+  // Calculate new coordinates given coordinates(lat,lon) and change_in_coordinates(lat,lon).
+  new_lat = old_coordinates[0]- change_in_coordinates[0]
+  new_lon = old_coordinates[1] - change_in_coordinates[1]
+
+  return [new_lat, new_lon]
+
+}
 
 
-  // console.log(path)
+function get_new_coordinates(old_coordinates, distance_moved, direction){
+    // Get new coordinates given old coordinates (lat,lon), distance moved in kilometers.
+    // direction of movement [N, S, E, W].
+
+    // Convert directions if needed
+    updated_data = convert_direction_to_north_or_west(distance_moved, direction)
+    distance_moved = updated_data["distance_moved"]
+    direction = updated_data["direction"]
+
+    if (direction == "N"){
+      latitude_change = change_in_latitude(distance_moved)
+      change_in_coordinates = [latitude_change, 0]
+    }
+    else if (direction == "W"){
+        latitude = old_coordinates[0]
+        longitude_change = change_in_longitude(latitude, distance_moved)
+        change_in_coordinates = [0, longitude_change]
+      }
+    else{
+
+        throw "Not a valid direction of movement! Please use one of  ['N', 'S', 'E', 'W']"
+      }
+
+    return gen_new_coordinates_from_change_in_coordinates(old_coordinates, change_in_coordinates)
+}
+
+function gen_square_path(controller_coordinates, dimension){
+  // Generate a square path around central controller for area of interest.
+  path = []
+  path.push(get_new_coordinates(controller_coordinates, dimension, "N"))
+  path.push(get_new_coordinates(controller_coordinates, dimension, "E"))
+  path.push(get_new_coordinates(controller_coordinates, dimension, "S"))
+  path.push(get_new_coordinates(controller_coordinates, dimension, "W"))
+
+  console.log(path);
   return path;
 }
 
-function get_center(area_of_interest) {
-  lat = (area_of_interest["lower_left"][0] + area_of_interest["upper_right"][0]) / 2;
-  lng = (area_of_interest["lower_left"][1] + area_of_interest["upper_right"][1]) / 2;
+// gen_square_path(get_central_controller_location(), 5)
 
-  return {
-    "lat": lat,
-    "lng": lng
-  };
+function add_markers(map, coordinates, icon){
+  // Adds markers to the map given a list of coordinates and icon url.
+  for (i = 0; i < coordinates.length; i++) {
+    map.addMarker({
+      lat: coordinates[i][0],
+      lng: coordinates[i][1],
+      title: String(coordinates[i][0]) + " ," + String(coordinates[i][1]),
+      icon: icon,
+      // icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/3by2white.svg/150px-3by2white.svg.png",
+    });
+  }
+}
+
+
+function draw_polygon(map, path){
+  // Draw a polygon on the map
+  polygon = map.drawPolygon({
+    paths: path, // pre-defined polygon shape
+    strokeColor: '#2c3e50',
+    strokeOpacity: 1,
+    strokeWeight: 3,
+    fillColor: '#FFF0',
+    fillOpacity: 0.6
+  });
 
 }
-var center = get_center(get_area_of_interest())
 
-var map = new GMaps({
-  el: '#map',
-  lat: center["lat"],
-  lng: center["lng"]
-});
-
-
-
-// Map overlays won't work
-//
-// var getTile = function(coord, zoom, ownerDocument) {
-//   var div = ownerDocument.createElement('div');
-//   div.innerHTML = coord;
-//   div.style.width = this.tileSize.width + 'px';
-//   div.style.height = this.tileSize.height + 'px';
-//   div.style.fontSize = '10';
-//   div.style.fontWeight = 'bolder';
-//   div.style.border = 'dotted 1px #aaa';
-//   div.style.textAlign = 'center';
-//   div.style.lineHeight = this.tileSize.height + 'px';
-//   return div;
-// };
-//
-// map.addOverlayMapType({
-//   index: 0,
-//   tileSize: new google.maps.Size(64, 64),
-//   getTile: getTile
-// });
-
-
-
-
-
-map.addMarker({
-  lat: center["lat"],
-  lng: center["lng"],
-  title: 'Central Server',
-  icon: "http://i.picresize.com/images/2017/07/24/R2Izc.png",
-  click: function(e) {
-    alert('You clicked in this marker');
+function add_controller_marker(map, center){
+  // Add central controller to the map
+  map.addMarker({
+    lat: center[0],
+    lng: center[1],
+    title: 'Central Server ' + "Lat:"+ center[0] + ", Lng:" + center[1],
+    icon: "http://i.picresize.com/images/2017/07/27/1a5k.png",
+    draggable: true,
+    dragend: function(event) {
+      handle_controller_marker_drag(event);
   }
 });
-
-
-// map.fitZoom();
-console.log(get_area_of_interest())
-var path = gen_square(get_area_of_interest());
-for (i = 0; i < path.length; i++) {
-  map.addMarker({
-    lat: path[i][0],
-    lng: path[i][1],
-    title: String(path[i][0]) + " ," + String(path[i][1]),
-    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/3by2white.svg/150px-3by2white.svg.png",
-  });
 }
-map.fitZoom();
-// var
 
-console.log(path)
-polygon = map.drawPolygon({
-  paths: path, // pre-defined polygon shape
-  strokeColor: '#2c3e50',
-  strokeOpacity: 1,
-  strokeWeight: 3,
-  fillColor: '#FFF0',
-  fillOpacity: 0.6
+function handle_controller_marker_drag(event){
+  // Handle the controller marker dragging event.
+  var lat = event.latLng.lat();
+  var lng = event.latLng.lng();
+  var new_controller_location = [lat, lng];
+  map.removePolygons();
+  map.removeMarkers();
+
+  // Update the controller location
+  update_central_controller_location(new_controller_location)
+  // Generate square path
+  var path = gen_square_path(new_controller_location, 10)
+  add_markers(map, path, "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/3by2white.svg/150px-3by2white.svg.png")
+  draw_polygon(map, path);
+  add_controller_marker(map, new_controller_location);
+  map.fitZoom();
+
+}
+
+var center = get_central_controller_location();
+
+// Initialize map with central controller as center point.
+var map = new GMaps({
+  el: '#map',
+  lat: center[0],
+  lng: center[1]
 });
+
+
+// Add central controller draggable marker to the map
+var path = gen_square_path(center, 10);
+add_markers(map, path, "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/3by2white.svg/150px-3by2white.svg.png")
+draw_polygon(map, path);
+add_controller_marker(map, center);
+map.fitZoom();
